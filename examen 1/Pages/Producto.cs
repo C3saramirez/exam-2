1c6489f6e7271beaf82e13a662f17487bf5a3315
c3saramirez <cramirez55@ucol.mx>
2024-04-17 13:34:44 -0600

    examen

---
+++ examen 1/Pages/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Producto
+{
+    [Required(ErrorMessage = "NO DEBE ESTAR VACIO")]
+    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "NO DEBE ESTAR VACIO")]
+
+    [Range(1, int.MaxValue, ErrorMessage = "DEBE ESER MAYOR A 0")]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "SELECCIONA UN DEPARTAMENTO")]
+    public string? Departamento { get; set; }
+}
